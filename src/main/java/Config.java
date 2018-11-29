af4983c7a76f3d546de29e7c72c16ac784988eee
Julia Panteley <ponteleju@gmail.com>
2018-11-30 01:47:23 +0300

    initial commit

---
+++ src/main/java/Config.java
@@ -0,0 +1,8 @@
+    public class Config {
+
+        private static final String CURRENT_DIRECTORY = System.getProperty("user.dir");
+        private static final String DS = System.getProperty("file.separator");
+        public static final String TEST_RESOURCES = CURRENT_DIRECTORY + DS + "src" + DS + "test" + DS + "resources";
+        public static final String DRIVERS = CURRENT_DIRECTORY + DS + "drivers";
+
+    }
